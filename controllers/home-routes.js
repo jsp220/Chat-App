e36c98b3641d71efcf1c81bce94115d7a55b28d7
Joon Park <jsp220@gmail.com>
2022-10-14 23:12:11 -0700

    Add functionality to alert users when they receive a message

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,29 +4,56 @@ const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, async (req,res) => {
     try {
-        const userChannelData = await UserChannel.findAll({
-            where: {
-                userId: req.session.user_id
-            }
-        })
-
         const userId = req.session.user_id;
-
+        
+        const userChannelData = await UserChannel.findAll({ where: {userId} })
         const userChannels = userChannelData.map((data) => data.get({ plain: true }));
 
-        const userData = await User.findAll();
-
+        const userData = await User.findAll({
+            attributes: {
+                exclude: ['password']
+            }
+        });
         const allUsers = userData.map((data) => data.get({ plain: true }));
-        
         const users = allUsers.filter((user) => user.id != userId);
+
+        let channels = [];
+
+        for (let i in userChannels) {
+            const channelData = await Channel.findByPk(userChannels[i].channelId, {
+                include: [
+                    {model: User, through: UserChannel, as: 'channel_user', attributes: {exclude: ['password']} }
+                ]
+            });
+
+            channels.push(channelData.get({ plain: true }));
+            // console.log(channel)
+        }
+
+        console.log(channels);
 
         let channel = [];
 
-        for (let i=0; i < userChannels.length; i++) {
-            const channelData = await Channel.findByPk(userChannels[i].channelId);
-            channel.push(channelData.get({ plain: true }));
-            // console.log(channel)
+        for (let i in channels) {
+            const id = channels[i].id;
+            const name = channels[i].name;
+            const user1 = req.session.username;
+            let user2;
+            if (channels[i].channel_user[0].username != user1) {
+                user2 = channels[i].channel_user[0].username;
+            } else {
+                user2 = channels[i].channel_user[1].username;
+            }
+            
+            // let user2;
+            // for (let j in channels[i].channel_user) {
+            //     if (channels[i].channel_user[j].username != req.session.username) {
+            //         user2 = channel[i].channel_user[j].username;
+            //     }
+            // }
+            channel.push({id, name, user1, user2});
         }
+        console.log(channel);
 
         res.render('home', {channel, users, userId, logged_in: req.session.logged_in})
     } catch (err) {
@@ -46,19 +73,19 @@ router.get('/channel/:id', withAuth, async (req, res) => {
         const userId = req.session.user_id;
         const userChannels = userChannelData.map((data) => data.get({ plain: true }));
 
-        
-
-        let channel = [];
+        let channels = [];
         let channelBelong = false;
         // create an array called channel 
-        for (let i=0; i < userChannels.length; i++) {
-            const channelData = await Channel.findByPk(userChannels[i].channelId);
-            channel.push(channelData.get({ plain: true }));
-            // console.log(userChannels[i].channelId);
-            // console.log(req.params.id);
+        for (let i in userChannels) {
+            const channelData = await Channel.findByPk(userChannels[i].channelId, {
+                include: [
+                    {model: User, through: UserChannel, as: 'channel_user', attributes: {exclude: ['password']} }
+                ]
+            });
+            channels.push(channelData.get({ plain: true }));
+
             if (userChannels[i].channelId == req.params.id) {
-                // console.log(userChannels[i].channelId);
-                // console.log(req.params.id);
+
                 channelBelong = true;
                 console.log(channelBelong);
             } 
@@ -66,10 +93,33 @@ router.get('/channel/:id', withAuth, async (req, res) => {
 
         if (channelBelong) {
 
+            let channel = [];
+
+            for (let i in channels) {
+                const id = channels[i].id;
+                const name = channels[i].name;
+                const user1 = req.session.username;
+                let user2;
+                if (channels[i].channel_user[0].username != user1) {
+                    user2 = channels[i].channel_user[0].username;
+                } else {
+                    user2 = channels[i].channel_user[1].username;
+                }
+                
+                // let user2;
+                // for (let j in channels[i].channel_user) {
+                //     if (channels[i].channel_user[j].username != req.session.username) {
+                //         user2 = channel[i].channel_user[j].username;
+                //     }
+                // }
+                channel.push({id, name, user1, user2});
+            }
+
+            console.log(channel);
+
             const singleChannelData = await Channel.findByPk(req.params.id, {
                 include: [
                     {model: User, through: UserChannel, as: 'channel_user', attributes: {exclude: ['password']} },
-                    // {model: User, through: UserChannel, as: 'channel_user'},
                     {model: Message, include: [
                         {model: User, attributes: {exclude: ['password']} }
                     ]}
--- public/js/newmessage.js
+++ public/js/newmessage.js
@@ -42,59 +42,56 @@ form.addEventListener('submit', async function (e) {
     }
 });
 
-socket.on('chat message', async function (msg, info) {
-    const sender = info.sender;
-    const receiver = info.receiver;
-    const utcDate = new Date(`${info.timestamp}`)
-    const timestamp = `${utcDate.toLocaleDateString()} ${utcDate.toLocaleTimeString('en-US')}`;
-    // console.log(timestamp)
+// socket.on('chat message', async function (msg, info) {
+//     const sender = info.sender;
+//     const receiver = info.receiver;
+//     const utcDate = new Date(`${info.timestamp}`)
+//     const timestamp = `${utcDate.toLocaleDateString()} ${utcDate.toLocaleTimeString('en-US')}`;
+//     // console.log(timestamp)
     
-    if (
-        (sender == localStorage.getItem('user1') && receiver == localStorage.getItem('user2')) ||
-        (sender == localStorage.getItem('user2') && receiver == localStorage.getItem('user1'))) {
+//     if (
+//         (sender == localStorage.getItem('user1') && receiver == localStorage.getItem('user2')) ||
+//         (sender == localStorage.getItem('user2') && receiver == localStorage.getItem('user1'))) {
 
-        var div1 = document.createElement('div');
-        if (sender == localStorage.getItem('username')) {
-            div1.classList.add('card', 'text-bg-primary', 'mb-3', 'right');
-        } else {
-            div1.classList.add('card', 'text-bg-success', 'mb-3', 'left');
-        }
+//         var div1 = document.createElement('div');
+//         if (sender == localStorage.getItem('username')) {
+//             div1.classList.add('card', 'text-bg-primary', 'mb-3', 'right');
+//         } else {
+//             div1.classList.add('card', 'text-bg-success', 'mb-3', 'left');
+//         }
         
-        var div2 = document.createElement('div');
-        div2.classList.add('card-header');
+//         var div2 = document.createElement('div');
+//         div2.classList.add('card-header');
         
-        var p1 = document.createElement('p');
-        p1.classList.add('fw-bolder', 'user-name');
-        p1.textContent = sender;
+//         var p1 = document.createElement('p');
+//         p1.classList.add('fw-bolder', 'user-name');
+//         p1.textContent = sender;
         
-        var p2 = document.createElement('p');
-        p2.classList.add('time-stamp');
-        p2.textContent = timestamp;
+//         var p2 = document.createElement('p');
+//         p2.classList.add('time-stamp');
+//         p2.textContent = timestamp;
 
-        var div3 = document.createElement('div');
-        div3.classList.add('card-body');
-        var item = document.createElement('p');
-        item.classList.add('card-title');
-        item.textContent = msg;
+//         var div3 = document.createElement('div');
+//         div3.classList.add('card-body');
+//         var item = document.createElement('p');
+//         item.classList.add('card-title');
+//         item.textContent = msg;
     
-        div2.appendChild(p1);
-        div2.appendChild(p2);
+//         div2.appendChild(p1);
+//         div2.appendChild(p2);
 
-        div3.appendChild(item);
+//         div3.appendChild(item);
 
-        div1.appendChild(div2);
-        div1.appendChild(div3);
+//         div1.appendChild(div2);
+//         div1.appendChild(div3);
 
-        channelWindow.appendChild(div1);
-    }
+//         channelWindow.appendChild(div1);
+//     } else if (receiver == localStorage.getItem('username')) {
+//         chatEl = document.querySelector(`chat-${sender}`);
+//         chatEl.setAttribute('style', 'background-color: red');
+//     }
 
-    // // brian's
-    // var item = document.createElement('li');
-    // item.textContent = msg;
-    // channelWindow.appendChild(item);
-    // // end brian's
-
-    rightCol.scrollTo(0, rightCol.scrollHeight);
-});
+//     rightCol.scrollTo(0, rightCol.scrollHeight);
+// });
 
 init();
