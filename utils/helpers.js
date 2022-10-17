74d157eb3fb7f726eafeedd4457d815e875f4489
Joon Park <jsp220@gmail.com>
2022-10-17 11:08:52 -0700

    Local time

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,7 @@
 module.exports = {
   format_date: utcDate => {
-    const date = new Date(utcDate);
+    const pacTime = Date.parse(utcDate) - 25200000;
+    const date = new Date(pacTime);
     return `${date.toLocaleDateString()} ${date.toLocaleTimeString('en-US')}`;
   }
 };
