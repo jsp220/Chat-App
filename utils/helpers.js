3bbcac2a1fd8388f1d629ead51f1768379f6caa7
Joon Park <jsp220@gmail.com>
2022-12-19 11:33:05 -0800

    adjust time zone difference

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,8 @@
 module.exports = {
   format_date: utcDate => {
-    const localTime = Date.parse(utcDate) - utcDate.getTimezoneOffset()*60*1000;
-    console.log(localTime);
-    // const pacTime = Date.parse(utcDate) - 28800000;
+    // const localTime = Date.parse(utcDate) - utcDate.getTimezoneOffset()*60*1000;
+    const localTime = Date.parse(utcDate) - 28800000;
+    // console.log(localTime);
     const date = new Date(localTime);
     return `${date.toLocaleDateString()} ${date.toLocaleTimeString('en-US')}`;
   }
